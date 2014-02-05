a0e2408dfddebd09376e463e50151679b490e8d2
Riter <riter.cordova@gmail.com>
2014-02-05 15:12:07 -0400

    control slider admin por plugin

--- wp-config.php
+++ wp-config.php
@@ -16,8 +16,8 @@
 
 // ** Ajustes de MySQL. Solicita estos datos a tu proveedor de alojamiento web. ** //
 /** El nombre de tu base de datos de WordPress */
-//define('DB_NAME', 'validoc_testwordpress');
-define('DB_NAME', 'testwordpress');
+define('DB_NAME', 'validoc_testwordpress');
+//define('DB_NAME', 'testwordpress');
 
 /** Tu nombre de usuario de MySQL */
 define('DB_USER', 'validoc');
