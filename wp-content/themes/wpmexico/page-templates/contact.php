6d767b346f336cc7be937ecfaa69290f544c1f65
Riter <riter.cordova@gmail.com>
2014-02-14 10:21:43 -0400

    form contact pugin

--- wp-content/themes/wpmexico/page-templates/contact.php
+++ wp-content/themes/wpmexico/page-templates/contact.php
@@ -15,22 +15,39 @@
             <div class="contacto">
                 <h2>Contacto</h2>
                 <div class="box-contacto">
-                    <form action="#">
+                    <!--<form action="#" method="post">-->
                         <?php
                         /*$page_data = get_page( get_the_ID() );
                         $page_data->post_content;
                         */
-                          echo do_shortcode('[contact-form-7 id="576" title="Formulario de contacto 1"]');
+                          echo do_shortcode('[contact-form-7 id="436" title="Formulario de contacto 1"]');
                         ?>
                         <!--<ul>
-                            <li><label for="nombre">Nombre</label><input id="nombre" type="text"></li>
-                            <li><label for="email">Email</label><input id="email" type="text"></li>
-                            <li><label for="telefono">Teléfono</label><input id="telefono" type="text"></li>
-                            <li><label for="mensaje">Mensaje</label><textarea name="mensaje" id="mensaje"></textarea></li>
-                            <li><input type="radio" name="enviar_copia" value="enviar_copia" id="enviar_copia" ><label for="enviar_copia">Enviarme una copia</label></li>
-                            <li class="last"><input type="submit" value="Enviar"></li>
+                            <li>
+                                <label for="nombre">Nombre</label>
+                                <input id="nombre" type="text">
+                            </li>
+                            <li>
+                                <label for="email">Email</label>
+                                <input id="email" type="text">
+                            </li>
+                            <li>
+                                <label for="telefono">Teléfono</label>
+                                <input id="telefono" type="text">
+                            </li>
+                            <li>
+                                <label for="mensaje">Mensaje</label>
+                                <textarea name="mensaje" id="mensaje"></textarea>
+                            </li>
+                            <li>
+                                <input type="radio" name="enviar_copia" value="enviar_copia" id="enviar_copia" >
+                                <label for="enviar_copia">Enviarme una copia</label>
+                                </li>
+                            <li class="last">
+                                <input type="submit" value="Enviar">
+                            </li>
                         </ul>-->
-                    </form>
+                    <!--</form>->-->
                     <ul>
                         <li><span><a class="fb" href="#">/JournalMexico</a></span></li>
                         <li><span><a class="tw" href="#">@JournalMexico</a></span></li>
